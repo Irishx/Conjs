f5fb6a87534b64842f48b0f8033f0ecd7132a0cb
Irishx <iris@i-hx.nl>
2014-01-03 10:24:15 +0100

    two R scripts for running LSA added.

---
+++ myrscript_sonarnews_top30k.r
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+cat("Hello")
+
+library(lsa)
+data(stopwords_nl)
+mySonarmatrix_d = textmatrix("/vol/tensusers/ihendrickx/Data/LSA/R_Sonar/Sonar10K1line/NewsFilt_top30K", language="dutch")
+spaceSonar = lsa( mySonarmatrix_d, 300)
+
+mydirlist = list.dirs("/vol/tensusers/ihendrickx/Data/LSA/R_Sonar/sonar_10knews/vocab_top30K/wantQP/")
+for (i in 2:length(mydirlist)){ 
+ wantmatrix = textmatrix(mydirlist[i],vocabulary=rownames(mySonarmatrix_d))
+ myfoldmatrix = fold_in(wantmatrix ,spaceSonar)
+ mycos = cosine(myfoldmatrix[,1],myfoldmatrix[,2])
+ setwd(mydirlist[i])
+ write(mycos, file="computed_cosine.txt") 
+}
+
+
